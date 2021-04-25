df3c065c9e6442e89627fa0347bca44d2f4b93b9
sanjarcode <sanjarcode@gmail.com>
2021-04-25 07:40:16 +0530

    Add init files

---
+++ zimrevise.py
@@ -0,0 +1,8 @@
+from zim.plugins import PluginClass
+
+class ZimRevisePlugin(PluginClass):
+    plugin_info = {
+      'name': _('Zim Revise'),
+      'description': _('Revise notes in multiple sittings'),
+      'author': 'Muhammad Sanjar Afaq',
+    }
